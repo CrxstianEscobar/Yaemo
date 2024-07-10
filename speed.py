6081cf5b0803b873d18b4623179a8cbd7af2d59d
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-07-10 15:50:28 -0500

    YaemoriBot-MD

--- speed.py
+++ speed.py
@@ -1965,7 +1965,7 @@ def shell():
                 ((results.upload / 1000.0 / 1000.0) / args.units[1],
                  args.units[0]),
                 quiet)
-        printer("\n...................................................................................\n▶︎ POWERED BY *OOKLA*\n▶︎ Script By *AzamiJs*")
+        printer("\n...................................................................................\n▶︎ POWERED BY *OOKLA*\n▶︎ Script By *KatashiFukushima*")
     else:
         printer('Saltando la prueba de carga', quiet)
 
