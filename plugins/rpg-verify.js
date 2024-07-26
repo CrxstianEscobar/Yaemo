4f9fb0d9192d3e712bbd4876a0f774e5c28736f4
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-07-25 22:12:40 -0500

    YaemoriBot-MD

--- plugins/rpg-verify.js
+++ plugins/rpg-verify.js
@@ -4,34 +4,32 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
 let user = global.db.data.users[m.sender]
 let name2 = conn.getName(m.sender)
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/7f9cbd5335d9f9a3e7860.jpg')
-if (user.registered === true) throw `*⚠️ Ya estás registrado*\n\n¿Quiere volver a registrarse?\n\n💬 Use este comando para *eliminar su registro*\n*${usedPrefix}unreg* <Número de serie>`
-if (!Reg.test(text)) throw `*⚠️ Formato incorrecto*\n\n📝 Uso del comamdo: *${usedPrefix + command} nombre.edad*\n💡 Ejemplo : *${usedPrefix + command}* ${name2}.18`
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => yartexImg.getRandom())
+
+if (user.registered === true) return conn.sendMessage(m.chat, { text: `${dis}Ya esta registrado como *${user.name}*\n\nSi desea hacer un nuevo registro ✨ debe de usar el comando:\n*${usedPrefix}delregistro* \`Número de serie\`\n\n🙂 Si no conoce su número de serie, use el comando:\n*${usedPrefix}numserie*`, ...fake }, { quoted: m })
+if (!Reg.test(text)) return conn.reply(m.chat, `${dis}\`Uso correcto del comando:\`\n*${usedPrefix + command}* nombre.edad\n\n🩷 *Ejemplo:*\n*${usedPrefix + command}* Katashi.18`, m)
 let [_, name, splitter, age] = text.match(Reg)
-if (!name) throw '*📝 El nombre no puede estar vacío*'
-if (!age) throw '*📝 La edad no puede estar vacía*'
-if (name.length >= 30) throw '*⚠️ El nombre es demasiado largo*' 
+if (!name) return conn.reply(m.chat, `🫠 *No hemos encontrado su nombre, intente de nuevo.*`, m)
+if (!age) return conn.reply(m.chat, `🤔 *No hemos encontrado su edad, intente de nuevo.*`, m)
+if (name.length >= 31) return conn.reply(m.chat, `😩 *Use un nombre más corto por favor.*`, m)
 age = parseInt(age)
-if (age > 100) throw '*👴🏻 Wow el abuelo quiere jugar al bot*'
-if (age < 5) throw '*👀 hay un bebé jsjsj*'
+if (age >= 61) return conn.reply(m.chat, `🤷‍♀️ *Use una edad más joven por favor.*`, m)
+if (age <= 10) return conn.reply(m.chat, `😆 *Use una edad mayor por favor.*`, m)
 user.name = name.trim()
 user.age = age
 user.regTime = + new Date
 user.registered = true
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	
-m.react('📩') 
-let regbot = `🗃️ *R E G I S T R A D O* 🗃️
-🪁 *Nombre:* ${name}
-🎨 *Edad* : ${age} años
-🥏 *Numero de serie*:
-${sn}`
-await m.reply(regbot)
-// await conn.sendUrl(m.chat, regbot, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: pp, thumbnailUrl: pp, title: 'Registrado 📩', }})
-
+m.react('✨') 
+await conn.sendMessage(m.chat, { image: { url: pp }, caption: `*║⫘⫘⫘⫘⫘⫘✨*
+*║ ${dis}ＲＥＧＩＳＴＲＯ*
+*║ .・゜゜・・゜゜・．*
+*║* 💠 *Nombre* ${name}
+*║* 💠 *Edad* ${age} años
+*║* 💠 *Número de serie* \`${sn}\`
+*║⫘⫘⫘⫘⫘⫘✨*`, mentions: [m.sender], ...fake }, { quoted: m })
+  
 }
-handler.help = ['reg']
-handler.tags = ['rg']
-handler.command = ['verify', 'reg', 'verificar'] 
-
+handler.command = /^(ver(ify|ificar)|reg(istrar)?)$/i
 export default handler
