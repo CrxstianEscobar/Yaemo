355cb3bc45428a1fa5dbb802001fec2249344489
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-07-29 02:55:23 -0500

    YaemoriBot-MD

--- config.js
+++ config.js
@@ -95,7 +95,11 @@ global.md = 'https://github.com/KatashiFukushima/YaemoriBot-MD'
 global.paypal = 'https://paypal.me/KatashiF'
 global.git = 'https://github.com/KatashiFukushima'
 global.email = 'katashifukushima23@gmail.com'
-global.group = 'https://chat.whatsapp.com/C7LzujTmWjQ6j5CUaBPaFI' //Grupo ofc
+global.group = 'https://chat.whatsapp.com/KGGL6RWE7eAJRGrSn3QOYl'
+
+🙏🤗❣️
+
+https://whatsapp.com/channel/0029VaefZxt1t90Tn4rCv50X //Grupo ofc
 global.channel = 'https://whatsapp.com/channel/0029Va8GeVFAO7REOj3qnW37'
 global.linkSity = [yt, ig, md, paypal, git, channel, email]
 
