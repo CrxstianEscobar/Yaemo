3f0f683add4a605c98a153937528a0cd1ea5aba1
KatashiFukushima <luisarmandorodasyovera@gmail.com>
2024-08-01 14:45:59 -0500

    𝑲𝒂𝒕𝒂𝒔𝒉𝒊 𝑭𝒖𝒌𝒖𝒔𝒉𝒊𝒎𝒂

--- plugins/bot-ds.js
+++ plugins/bot-ds.js
@@ -27,8 +27,8 @@ break
 if (filesDeleted === 0) {
 await conn.reply(m.chat, '🚩 *No se encontró ningún archivo que incluya la ID del chat*', m, fake, )
 } else {
-await conn.reply(m.chat, `🎌 *Se eliminaron ${filesDeleted} archivos de sesión*`, m, fake, )
-conn.reply(m.chat, `😸 *¡Hola! logras verme*`, m, fake, )
+await conn.reply(m.chat, `♻️ *Se eliminaron ${filesDeleted} archivos de sesión* 💠`, m, fake, )
+conn.reply(m.chat, `🌸 *¡Hola! ¿logras verme?* 💙`, m, fake, )
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err)
